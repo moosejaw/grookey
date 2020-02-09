d2afa43981f0cd24471eac215bf52eb84846c650
moosejaw <joshdemir@gmail.com>
2020-02-09 21:49:31 +0000

    Return a response if promise is rejected

--- smogon/app.js
+++ smogon/app.js
@@ -5,14 +5,28 @@ I need zombie.js and express to run so please install
 those first.
 */
 // Set up the zombie browser
-//const Browser = require('zombie');
+const Browser = require('zombie');
 
 // Now the express stuff
 const express = require('express');
 const app     = express();
 
 app.get('/api/', function(req, res) {
-    res.send('Hi from Node!');
+    // Make a browser object
+    //let txt = "you shouldn't see this";
+    let metagame = req.query.metagame;
+    let pokemon  = req.query.pkmn;
+    let url = `http://www.smogon.com/dex/${metagame}/pokemon/${pokemon}`;
+
+    // Call the URL grabber function thingy
+    let browser = new Browser();
+    browser.visit(url).then(() => {
+        browser.pressButton('.ExportButton');
+        let txt = browser.text('textarea');
+        res.send(txt);
+    },
+    () => { res.send('404'); } // on reject
+    );
 })
 
 app.listen(process.env.PORT);
