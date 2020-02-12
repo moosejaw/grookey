4a55840f8fcffaf2b5e908dd514d97dddc892fba
moosejaw <joshdemir@gmail.com>
2020-02-12 10:47:55 +0000

    Get multiple movesets and titles

--- smogon/app.js
+++ smogon/app.js
@@ -11,22 +11,77 @@ const Browser = require('zombie');
 const express = require('express');
 const app     = express();
 
+function getTimeInSecs() {
+    let d = new Date();
+    return d.toTimeString();
+}
+
+// Create the browser object
+//var browser = new Browser();
+
 app.get('/api/', function(req, res) {
+    console.log(`Started responding to a req at ${getTimeInSecs()}`);
+
+    let browser = new Browser();
+    console.log(`Opened a new browser instance at ${getTimeInSecs()}`);
+
     // Make a browser object
-    //let txt = "you shouldn't see this";
     let metagame = req.query.metagame;
     let pokemon  = req.query.pkmn;
     let url = `http://www.smogon.com/dex/${metagame}/pokemon/${pokemon}`;
+    let resp = {msgs: [], titles: [], code: 404};
 
-    // Call the URL grabber function thingy
-    let browser = new Browser();
-    browser.visit(url).then(() => {
-        browser.pressButton('.ExportButton');
-        let txt = browser.text('textarea');
-        res.send(txt);
-    },
-    () => { res.send('404'); } // on reject
+    // Go to the smogon page
+    browser.visit(url).then(
+        () => {
+            console.log(`Loaded the page at ${getTimeInSecs()}`);
+
+            // Get the moveset data
+            let movesets = browser.queryAll('.ExportButton');
+            if (movesets.length > 0) {
+                // Push the button and copy/paste the textarea stuff
+                movesets.forEach((v, i) => {
+                    browser.pressButton(v);
+                    resp.msgs.push(browser.text('textarea'));
+                    browser.pressButton(v);
+                });
+
+                console.log(`Got the moveset data at ${getTimeInSecs()}`);
+
+                // Now do the ugly thing to get the moveset titles...
+                // TODO: Don't make this check all divs on the page, there must be a better way
+                let titles = new Set();
+                browser.queryAll('div').forEach((v, i) => {
+                    let btns  = v.getElementsByClassName('ExportButton');
+                    if (btns.length > 0) {
+                        let title = v.getElementsByTagName('h1').item(0).textContent;
+
+                        // ughhhhhh
+                        if (title.toLowerCase() != pokemon.toLowerCase() && title.toLowerCase() != 'overview') {
+                            titles.add(title);
+                        }
+                    }
+                });
+
+                console.log(`Done iterating through the divs at ${getTimeInSecs()}`);
+
+                // Push the titles into the array
+                titles.forEach((v, i) => { resp.titles.push(v); });
+
+                // Send the response
+                console.log(`Returning the request at ${getTimeInSecs()}`);
+
+                resp.code = 200;
+                res.send(resp);
+            }
+            else {
+                // No moveset data!
+                resp.code = 405;
+                res.send(resp);
+            }
+        },
+        () => { res.send(resp); }
     );
-})
+});
 
 app.listen(process.env.PORT);
