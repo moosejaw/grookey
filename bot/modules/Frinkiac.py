a0eca77ebe54faabfe9509bbb60c744b9dbce521
moosejaw <joshdemir@gmail.com>
2020-05-28 18:04:29 +0100

    remove a couple prints

--- bot/modules/Frinkiac.py
+++ bot/modules/Frinkiac.py
@@ -13,28 +13,24 @@ class Compuglobal:
         
 
     def getRandomPicURL(self, use_gif=False, use_caption=False, include_zombie=False):
+        sc = self.getScreencap()
+        url = sc.get_meme_url() if not use_gif else sc.get_gif_url()
         valid = False if not include_zombie else True
         while not valid:
-            sc = self.getScreencap()
-
-            # Use GIF if specified
-            if use_gif:
-                sc = sc.get_gif_url()
-            else:
-                sc = sc.get_meme_url()
-
-            season = re.match(r"\/S[0-9]{2}", sc).group(0)
-            print(f'the match was {season} and the url was {sc}')
+            season = re.search(r"\/S[0-9]{2}", url).group(0)
+            print(f'the match was {season} and the url was {url}', flush=False)
             season = int(season.split('/S')[1])
             
-            print(f'processed season is {season} and threshold is {self.zombie_threshold}')
+            print(f'processed season is {season} and threshold is {self.zombie_threshold}', flush=False)
             if season <= self.zombie_threshold:
                 valid = True
                 print(f'pic ok! sending...')
             else:
-                print('pic is from zombie simpsons, rerolling...')
+                sc = self.getScreencap()
+                url = sc.get_meme_url()
+                print('pic is from zombie simpsons, rerolling...', flush=False)
         
         # Split the URL to remove caption
         if not use_caption:
-            sc = sc.split('?')[0]
-        return sc
+            url = url.split('?')[0]
+        return url
