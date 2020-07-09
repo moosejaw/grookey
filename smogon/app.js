928ac48b859cbdcddedd7b576f7502d9494b91f1
moosejaw <joshdemir@gmail.com>
2020-07-09 14:18:35 +0100

    Get formats of pokemon movesets

--- smogon/app.js
+++ smogon/app.js
@@ -17,16 +17,14 @@ function getTimeInSecs() {
 }
 
 
-app.get('/api/', function(req, res) {
+app.get('/movesets/', (req, res) => {
     console.log(`Started responding to a req at ${getTimeInSecs()}`);
 
     let browser = new Browser();
     console.log(`Opened a new browser instance at ${getTimeInSecs()}`);
 
     // Make a browser object
-    let metagame = req.query.metagame;
-    let pokemon  = req.query.pkmn;
-    let url = `http://www.smogon.com/dex/${metagame}/pokemon/${pokemon}`;
+    let url = `http://www.smogon.com/dex/${req.query.metagame}/pokemon/${req.query.pkmn}`;
     let resp = {
         data: [],
         titles: [],
@@ -52,14 +50,6 @@ app.get('/api/', function(req, res) {
                 }
             }
 
-            /*
-            let pageTypes = document.getElementsByClassName('.TypeList').children;
-            for (let item of pageTypes) {
-                console.log(`the item is: ${item}`);
-                resp.types.push(item.innerText.toLowerCase()); 
-            }
-            */
-
             // Get the moveset data
             let movesets = browser.queryAll('.ExportButton');
             if (movesets.length > 0) {
@@ -84,15 +74,15 @@ app.get('/api/', function(req, res) {
                 });
 
                 // Now do the ugly thing to get the moveset titles...
-                // TODO: Don't make this check all divs on the page, there must be a better way
+                // TODO: Don't make this check every single div on the page, there must be a better way
                 let titles = new Set();
                 browser.queryAll('div').forEach((v, i) => {
                     let btns  = v.getElementsByClassName('ExportButton');
                     if (btns.length > 0) {
                         let title = v.getElementsByTagName('h1').item(0).textContent;
 
-                        // ughhhhhh
-                        if (title.toLowerCase() != pokemon.toLowerCase() && title.toLowerCase() != 'overview') {
+                        // This is probably kind of bad?
+                        if (title.toLowerCase() != req.query.pkmn && title.toLowerCase() != 'overview') {
                             titles.add(title);
                         }
                     }
@@ -119,6 +109,39 @@ app.get('/api/', function(req, res) {
             console.log(`Rejected promise (404) at ${getTimeInSecs()}`);
             console.log(`Returning ${JSON.stringify(resp)}`);
             res.send(resp); 
+        }
+    );
+});
+
+app.get('/formats/', (req, res) => {
+    // Setup
+    let browser = new Browser();
+    let url = `http://www.smogon.com/dex/${req.query.metagame}/pokemon/${req.query.pkmn}`;
+    let resp = {
+        data: [],
+        url: url,
+        code: 404
+    };
+
+    browser.visit(url).then(
+        // Fulfilled
+        () => {
+            let div = browser.queryAll('.PokemonPage-StrategySelector');
+            if (div.length == 0) { resp.code = 405; } 
+            else {
+                // div with class name -> ul (children[2])-> li (children)
+                for (let item of div[0].children[2].children) {
+                    resp.data.push(item.textContent.toLowerCase())
+                }
+                resp.code = 200;
+            }
+            res.send(resp);
+            
+        },
+        // Rejected
+        () => {
+            resp.code = 404;
+            res.send(resp);
         }
     );
 });
