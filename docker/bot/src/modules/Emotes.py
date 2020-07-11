676af4a593455af371f9437c8f1eff57efee5854
moosejaw <joshdemir@gmail.com>
2020-07-11 18:10:59 +0100

    Rename emoji to emotes

---
+++ docker/bot/src/modules/Emotes.py
@@ -0,0 +1,10 @@
+class Emotes:
+    '''Contains lookups for custom emotes used by the bot.'''
+    def __init__(self):
+        self.stat_emotes = {
+            'r': r'<:stat_bar_r:731497549822951485>',
+            'o': r'<:stat_bar_o:731497549680214138>',
+            'y': r'<:stat_bar_y:731497549818757171>',
+            'pg': r'<:stat_bar_pg:731497549508116503>',
+            'bg': r'<:stat_bar_bg:731497549784940576>'
+        }
